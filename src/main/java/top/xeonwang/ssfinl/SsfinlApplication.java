f5b2b5ae83c6228a16fab55d205585df852aa8fd
XeonWangYue <qiwang18@qq.com>
2021-05-11 14:22:42 +0800

    create

---
+++ src/main/java/top/xeonwang/ssfinl/SsfinlApplication.java
@@ -0,0 +1,13 @@
+package top.xeonwang.ssfinl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfinlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsfinlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/xeonwang/ssfinl/SsfinlApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xeonwang.ssfinl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfinlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
